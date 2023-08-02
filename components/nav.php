88edb43a96516f5a6b080548a7441500ef8672ec
Franck <113984366+FranckASSOBO@users.noreply.github.com>
2023-08-02 16:45:44 +0200

    ajout de la navbar

--- components/nav.php
+++ components/nav.php
@@ -1,3 +1,26 @@
-<nav>
-    <a href="">Repertoire</a>
+<!-- <nav> -->
+<nav class="navbar bg-body-tertiary ">
+    <div class="container-fluid bg-success ">
+        <a class="navbar-brand" href="#">
+            <img src="../images/Beauty shop.jpg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top ">
+            Mon Repertoire de film
+        </a>
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+        </ul>
+    </div>
 </nav>
+<!-- <a href="">Repertoire</a> -->
+
+<!-- </nav> -->
